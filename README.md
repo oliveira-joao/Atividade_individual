8b39378982c91501efb819b29809d326565f145b
João Oliveira <joao.vsilva@bandtec.com.br>
2020-11-27 01:36:21 -0300

    bastante coisa

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Atividade_individual
+# projeto-modelo-individual-1ads-202
+Projeto Modelo para o Projeto Individual para as turmas de 1º Semestre - ADS - 2020/2 - Bandtec Digital School
+
+## Acesse o tutorial
+Indo em projeto-site/O-QUE-FAZER-AQUI.txt
--- projeto-site/config/config.js
+++ projeto-site/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   production: {
-    username: 'SEU_USUARIO_DO_BANCO_DE_DADOS',
-    password: 'SUA_SENHA_DO_BANCO_DE_DADOS',
-    database: 'NOME_DO_SEU_BANCO_DE_DADOS',
-    host: 'URL_DO_SEU_BANCO_DE_DADOS.database.windows.net',
+    username: 'joaovitor',
+    password: '112125Amor',
+    database: 'individual',
+    host: 'individual.database.windows.net',
     dialect: 'mssql',
     xuse_env_variable: 'DATABASE_URL',
     dialectOptions: {
